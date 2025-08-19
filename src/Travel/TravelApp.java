5bec47ee6ae498465da9b8cab2bf53716e4d1583
Yoo Wonsang <dnjstkd04@naver.com>
2025-08-19 18:41:35 +0900

    skel_틀 + 페이지 구현

--- src/Travel/TravelApp.java
+++ src/Travel/TravelApp.java
@@ -5,52 +5,52 @@ import java.util.Scanner;
 
 public class TravelApp {
     public static void main(String[] args) {
-        TravelDao doa =  new TravelDao("jdbc:mysql://localhost:3306/skel","root","1234");
-        ArrayList<TravelVo> rs = null;
-
-        rs = doa.executeQuery("select * from travel where no between 1 and 10");
-
-        for (TravelVo r : rs) {
-            String district = r.getDistrict();
-            String title = r.getTitle();
-            String description = r.getDescription();
-            String address = r.getAddress();
-            String phone = r.getPhone();
-            System.out.println(district + " " + title + " " + description + " " + address + " " + phone);
-        }
-
-
-
-//        try (Scanner scanner = new Scanner(System.in)) { // Scanner를 try-with-resources로 선언하여 자동으로 닫히게 설정
-//            TravelService ts = new  TravelService(scanner);
-//            while (true) { // 무한 루프를 사용하여 메뉴 반복 출력
-//                // 메뉴 출력
-//                System.out.println("\n=== TravelApp Menu ===");
-//                System.out.println("1. 전체목록(테이블 형태로 출력)");
-//                System.out.println("2. 권역별 관광지");
-//                System.out.println("3. 검색");
-//                System.out.println("4. Exit");
-//                System.out.print("Choose an option: ");
-//                int choice = scanner.nextInt(); // 사용자로부터 메뉴 선택 입력 받음
-//                scanner.nextLine(); // 버퍼에 남아있는 줄바꿈 문자 제거
+//        TravelDao doa =  new TravelDao("jdbc:mysql://localhost:3306/skel","root","1234");
+//        ArrayList<TravelVo> rs = null;
 //
-//                switch (choice) { // 선택한 옵션에 따라 동작 분기
-//                    case 1:
-//                        ts.printAllPage();
-//                        break;
-//                    case 2:
-//                       ts.printArea();
-//                        break;
-//                    case 3:
-//                        ts.printSearchResult();
-//                        break;
-//                    case 4:
-//                        System.out.println("Exiting..."); // 종료 메시지 출력
-//                        return; // 프로그램 종료
-//                    default:
-//                        System.out.println("Invalid choice. Try again."); // 잘못된 입력 메시지 출력
-//                }
-//            }
+//        rs = doa.executeQuery("select * from travel where no between 1 and 10");
+//
+//        for (TravelVo r : rs) {
+//            String district = r.getDistrict();
+//            String title = r.getTitle();
+//            String description = r.getDescription();
+//            String address = r.getAddress();
+//            String phone = r.getPhone();
+//            System.out.println(district + " " + title + " " + description + " " + address + " " + phone);
 //        }
+
+
+
+        try (Scanner scanner = new Scanner(System.in)) { // Scanner를 try-with-resources로 선언하여 자동으로 닫히게 설정
+            TravelService ts = new  TravelService(scanner);
+            while (true) { // 무한 루프를 사용하여 메뉴 반복 출력
+                // 메뉴 출력
+                System.out.println("\n=== TravelApp Menu ===");
+                System.out.println("1. 전체목록(테이블 형태로 출력)");
+                System.out.println("2. 권역별 관광지");
+                System.out.println("3. 검색");
+                System.out.println("4. Exit");
+                System.out.print("Choose an option: ");
+                int choice = scanner.nextInt(); // 사용자로부터 메뉴 선택 입력 받음
+                scanner.nextLine(); // 버퍼에 남아있는 줄바꿈 문자 제거
+
+                switch (choice) { // 선택한 옵션에 따라 동작 분기
+                    case 1:
+                        ts.printAllPage();
+                        break;
+                    case 2:
+                       ts.printArea();
+                        break;
+                    case 3:
+                        ts.printSearchResult();
+                        break;
+                    case 4:
+                        System.out.println("Exiting..."); // 종료 메시지 출력
+                        return; // 프로그램 종료
+                    default:
+                        System.out.println("Invalid choice. Try again."); // 잘못된 입력 메시지 출력
+                }
+            }
+        }
     }
 }
--- src/Travel/TravelService.java
+++ src/Travel/TravelService.java
@@ -1,15 +1,67 @@
 package Travel;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TravelService {
     Scanner sc = null;
+    TravelDao dao;
+    ArrayList<TravelVo> travels = null;
 
     TravelService(Scanner sc) {
         this.sc = sc;
+        dao =  new TravelDao("jdbc:mysql://localhost:3306/skel","root","1234");
     }
 
     void printAllPage(){
+        travels = dao.executeQuery("select * from travel where no between 1 and 20");
+        int page = 1;
+        int lastPage = travels.size()%5==0 ? travels.size()/5 : travels.size()/5+1;
+        int start = 0;
+        int end = 5<travels.size() ? 5:travels.size();
+        boolean flag = true;
+        while(flag){
+            for(int i=start;i<end;i++){
+                System.out.println(travels.get(i).getNo());
+                System.out.println("권역 : "+travels.get(i).getDistrict());
+                System.out.println("위치 : " + travels.get(i).getAddress());
+                System.out.println("명칭 : " + travels.get(i).getTitle());
+                System.out.println("상세정보 : " + travels.get(i).getDescription());
+                System.out.println("연락처 : " + travels.get(i).getAddress());
+                System.out.println("=================================================");
+            }
+            if(page==1){
+                System.out.print("              ");
+            }else{
+                System.out.print("<<이전 페이지(q입력)");
+            }
+            System.out.print("  page("+page+"/"+ lastPage+")  ");
+            if(page==lastPage){
+                System.out.print("              ");
+            }else{
+                System.out.print("다음 페이지(w입력)>>");
+            }
+            System.out.print("그만 보기는 e");
+            String c = sc.nextLine();
+            if(c.equals("q")){
+                if(page>1){
+                    page--;
+                    start = start-5;
+                    end = (end-5)%5==0 ? end-5: 5*(end/5);
+                }
+            }else if(c.equals("w")){
+                if(page<lastPage){
+                    page++;
+                    start = start+5;
+                    end = end + 5 <=travels.size() ? end + 5 :travels.size();
+                }
+            }
+            else if(c.equals("e")){
+                flag = false;
+            }
+        }
+
+
 
     }
 
